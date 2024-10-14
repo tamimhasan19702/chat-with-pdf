b800664eb80b29dd7dbfd37fc37b72957480c3d0
tamimhasan19702 <tamimhasancu@gmail.com>
2024-10-15 00:05:53 +0600

    update page.tsx

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
+
+export default clerkMiddleware({
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
