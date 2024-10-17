0222d94c965dba3875efcd15aa5f1482d34d06fd
tamimhasan19702 <tamimhasancu@gmail.com>
2024-10-17 20:55:10 +0600

    build(deps): update package-lock.json and package.json

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,20 @@
+/** @format */
+
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const chats = pgTable("chats", {
+  id: serial("id").primaryKey(),
+  pdfName: text("pdf_name").notNull(),
+  pdfUrl: text("pdf_url").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  userId: varchar("user_id", { length: 256 }).notNull(),
+  fileKey: text("file_key").notNull(),
+});
